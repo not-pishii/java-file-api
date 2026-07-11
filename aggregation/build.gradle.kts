7a94c267966b3c4714e7bf232d969b0cb8355e92
Egor Pishii <66114027+not-pishii@users.noreply.github.com>
2026-07-11 17:45:17 +0300

    ci: add code coverage limit (#10)

--- aggregation/build.gradle.kts
+++ aggregation/build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    base
     `test-report-aggregation`
     `jacoco-report-aggregation`
 }
@@ -15,5 +16,44 @@ reporting {
         create<JacocoCoverageReport>("testCodeCoverageReport") {
             testSuiteName = "test"
         }
+    }
+}
+
+tasks {
+    val testCodeCoverageReport = named<JacocoReport>("testCodeCoverageReport")
+
+    val jacocoAggregatedCoverageVerification =
+        register<JacocoCoverageVerification>("jacocoAggregatedCoverageVerification") {
+            group = "verification"
+
+            dependsOn(testCodeCoverageReport)
+            executionData.setFrom(testCodeCoverageReport.map { it.executionData })
+            classDirectories.setFrom(testCodeCoverageReport.map { it.classDirectories })
+            sourceDirectories.setFrom(testCodeCoverageReport.map { it.sourceDirectories })
+
+            violationRules {
+                rule {
+                    limit {
+                        counter = "LINE"
+                        minimum = "1.00".toBigDecimal()
+                    }
+                    limit {
+                        counter = "METHOD"
+                        minimum = "1.00".toBigDecimal()
+                    }
+                    limit {
+                        counter = "CLASS"
+                        minimum = "1.00".toBigDecimal()
+                    }
+                    limit {
+                        counter = "BRANCH"
+                        minimum = "0.98".toBigDecimal()
+                    }
+                }
+            }
+        }
+
+    check {
+        dependsOn(jacocoAggregatedCoverageVerification)
     }
 }
