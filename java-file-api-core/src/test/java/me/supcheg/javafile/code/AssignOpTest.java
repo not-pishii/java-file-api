d9f2b32af4462d98d5f6fafebef2d06d4c02c31f
not-pishii <not-pishii@ya.ru>
2026-07-20 20:46:02 +0300

    test: cover all AssignOp symbol values

--- java-file-api-core/src/test/java/me/supcheg/javafile/code/AssignOpTest.java
+++ java-file-api-core/src/test/java/me/supcheg/javafile/code/AssignOpTest.java
@@ -1,8 +1,12 @@
 package me.supcheg.javafile.code;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,5 +22,27 @@ class AssignOpTest {
     @Test
     void addAssignSymbolIsPlusEquals() {
         assertThat(AssignOp.ADD_ASSIGN.symbol()).isEqualTo("+=");
+    }
+
+    @ParameterizedTest
+    @MethodSource("symbols")
+    void symbolMatchesOperator(AssignOp op, String expectedSymbol) {
+        assertThat(op.symbol()).isEqualTo(expectedSymbol);
+    }
+
+    static Stream<Arguments> symbols() {
+        return Stream.of(
+                Arguments.of(AssignOp.ASSIGN, "="),
+                Arguments.of(AssignOp.ADD_ASSIGN, "+="),
+                Arguments.of(AssignOp.SUB_ASSIGN, "-="),
+                Arguments.of(AssignOp.MUL_ASSIGN, "*="),
+                Arguments.of(AssignOp.DIV_ASSIGN, "/="),
+                Arguments.of(AssignOp.MOD_ASSIGN, "%="),
+                Arguments.of(AssignOp.AND_ASSIGN, "&="),
+                Arguments.of(AssignOp.OR_ASSIGN, "|="),
+                Arguments.of(AssignOp.XOR_ASSIGN, "^="),
+                Arguments.of(AssignOp.SHL_ASSIGN, "<<="),
+                Arguments.of(AssignOp.SHR_ASSIGN, ">>="),
+                Arguments.of(AssignOp.USHR_ASSIGN, ">>>="));
     }
 }
